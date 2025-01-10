b318f0a70538ab7e16942a540a06f3877d9556e3
Ivan Castro <icastro@knowmadmood.com>
2025-01-10 19:07:03 +0100

    Definition of the DTO and response for the apirest layer.

---
+++ src/main/java/com/ecommerce/inditex/apirest/dto/PriceDTO.java
@@ -0,0 +1,30 @@
+package com.ecommerce.inditex.apirest.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceDTO {
+
+	/** The product id. */
+	private Integer productId;
+
+	/** The brand id. */
+	private Integer brandId;
+
+	/** The price. */
+	private Double price;
+
+	/** The rate id. */
+	private Integer rateId;
+
+	/** The application date. */
+	private List<LocalDate> applicationDates;
+}
