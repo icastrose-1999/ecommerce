f585b3e3e9fa972c6c8b208d666e95bc222d957e
Ivan Castro <icastro@knowmadmood.com>
2025-01-10 10:40:19 +0100

    Solved the compilation issues, created the auxiliary table for brandId, and set up the configuration for OpenAPI autogenerated model templates to include a builder.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,16 @@
+CREATE TABLE brands(
+	brand_id INTEGER PRIMARY KEY,
+	brand VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE prices (
-	rate_id BIGINT AUTO_INCREMENT PRIMARI KEY,
-	brand_id INTEGER FOREIGN KEY NOT NULL,
+	rate_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+	brand_id INTEGER NOT NULL,
 	start_date TIMESTAMP NOT NULL,
 	end_date TIMESTAMP NOT NULL,
 	product_id INTEGER NOT NULL,
 	priority INTEGER NOT NULL,
 	price DECIMAL(10,2),
-	curr CHAR(3)
+	curr CHAR(3),
+	CONSTRAINT fk_brand_id FOREIGN KEY (brand_id) REFERENCES brands(brand_id)
 );
