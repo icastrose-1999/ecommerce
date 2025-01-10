70f6749ba4aa5734dae5609c9e99287c248ac8d7
Ivan Castro <icastro@knowmadmood.com>
2025-01-10 19:06:13 +0100

    Implemented the error handling structure using @ControllerAdvice.

---
+++ src/main/java/com/ecommerce/inditex/apirest/controllers/ExceptionHandlerController.java
@@ -0,0 +1,73 @@
+package com.ecommerce.inditex.apirest.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import com.ecommerce.inditex.domain.exceptions.InvalidRequestException;
+import com.ecommerce.model.ErrorResponseDTO;
+
+/**
+ * The Class ExceptionHandlerController.
+ */
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+	/**
+	 * Handle invalid request.
+	 *
+	 * @param ex the ex
+	 * @return the response entity
+	 */
+	@ExceptionHandler(InvalidRequestException.class)
+	public ResponseEntity<ErrorResponseDTO> handleInvalidRequest(InvalidRequestException ex) {
+		
+		ErrorResponseDTO ErrorResponseDTO = new ErrorResponseDTO().builder()
+				.code( HttpStatus.BAD_REQUEST.value())
+				.title("Invalid Request")
+				.detail(ex.getMessage())
+				.build();
+				
+		return new ResponseEntity<>(ErrorResponseDTO, HttpStatus.BAD_REQUEST);
+	}
+
+	/**
+     * Handle type mismatch.
+     *
+     * @param ex the ex
+     * @return the response entity
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponseDTO> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        
+    	String detail = String.format("Parameter '%s' should be of type %s", ex.getName(), ex.getRequiredType().getSimpleName());
+       
+    	ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO().builder()
+        		.code( HttpStatus.BAD_REQUEST.value())
+        		.title("Type Mismatch")
+        		.detail(detail)
+				.build();
+
+        return new ResponseEntity<>(errorResponseDTO, HttpStatus.BAD_REQUEST);
+    }
+
+	/**
+	 * Handle unexpected error.
+	 *
+	 * @param ex the ex
+	 * @return the response entity
+	 */
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorResponseDTO> handleUnexpectedError(Exception ex) {
+		
+		ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO().builder()
+        		.code( HttpStatus.INTERNAL_SERVER_ERROR.value())
+        		.title("Internal server error")
+        		.detail(ex.getMessage())
+				.build();
+		
+		return new ResponseEntity<>(errorResponseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
+}
---
+++ src/main/java/com/ecommerce/inditex/domain/exceptions/InvalidRequestException.java
@@ -0,0 +1,15 @@
+package com.ecommerce.inditex.domain.exceptions;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * The Class InvalidRequestException.
+ */
+@AllArgsConstructor
+public class InvalidRequestException extends RuntimeException {
+
+	/** The Constant serialVersionUID. */
+	private static final long serialVersionUID = 1L;
+
+}
+
